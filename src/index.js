2843d00fcd53d222aa0515d162d3cbdf36bcee06
Ahmed <ahmedkhan895.ak@gmail.com>
2024-03-25 22:51:33 +0000

    added error handling for first page

--- src/index.js
+++ src/index.js
@@ -14,13 +14,17 @@ valuationIconElement.src = valuationIcon;
 document.getElementById("button1").addEventListener("click", function () {
   const inputField = document.querySelector("#postcodeInput");
   var postcodeInput = document.getElementById("postcodeInput").value.trim();
-  var postcodeRegex = /^[A-Z]{1,2}[0-9][A-Z0-9]? [0-9][A-Z]{2}$/i;
+  var postcodeRegex = /[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}/i;
+
   if (!postcodeRegex.test(postcodeInput)) {
     inputField.value = "";
-    inputField.placeholder = "please enter a valid postcode";
+    inputField.placeholder = "Please enter a valid UK postcode";
+    console.log("invalid");
   } else {
     // Perform other actions if postcode is valid
-    document.getElementById("error-message").style.display = "none";
     // For example, you might want to submit a form or initiate an API request here
+    inputField.value = "";
+    inputField.placeholder = "Enter your postcode";
+    console.log("valid");
   }
 });
