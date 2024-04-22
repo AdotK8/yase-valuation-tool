1643cd690d5e7f96fc1ab6d8510a17853ac9327a
Ahmed <ahmedkhan895.ak@gmail.com>
2024-04-22 23:13:37 +0100

    deployment of frontend to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "development",
@@ -30,4 +31,20 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    fallback: {
+      path: require.resolve("path-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      crypto: require.resolve("crypto-browserify"),
+      buffer: require.resolve("buffer/"),
+      stream: require.resolve("stream-browserify"),
+      vm: require.resolve("vm-browserify"),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
 };
